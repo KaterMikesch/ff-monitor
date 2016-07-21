6614b4ccb576f25bf3a45a3e743e25c8d4f62310
Axel Katerbau <axel@objectpark.org>
2016-07-21 19:31:12 +0200

    upped to alpha 10

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-bin "0.3.4"]]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha7"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [clj-time "0.11.0"]
                  [org.clojure/data.json "0.2.6"]
                  [com.draines/postal "1.11.3"]
